8fbe8b795910cd600a298d14ea7ce21ff19fd70d
Munirahenad <muneraenad@hotmail.com>
2026-07-04 05:15:00 +0300

    Create LoadingState.jsx

---
+++ Stage 4/frontend/src/components/LoadingState.jsx
@@ -0,0 +1,10 @@
+function LoadingState() {
+  return (
+    <div className="state-container">
+      <div className="loading-spinner" />
+      <p className="state-text">Loading...</p>
+    </div>
+  );
+}
+
+export default LoadingState;
