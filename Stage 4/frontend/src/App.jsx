dae3fd9dde6e5da391f9d18778418d4feeb39882
Munirahenad <muneraenad@hotmail.com>
2026-07-04 05:10:58 +0300

    Add main App component with routing setup

---
+++ Stage 4/frontend/src/App.jsx
@@ -0,0 +1,86 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Layout from "./components/Layout";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Devices from "./pages/Devices";
+import Alerts from "./pages/Alerts";
+import Readings from "./pages/Readings";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Layout title="Dashboard">
+              <Dashboard />
+            </Layout>
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/devices"
+        element={
+          <ProtectedRoute>
+            <Layout title="Devices">
+              <Devices />
+            </Layout>
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/alerts"
+        element={
+          <ProtectedRoute>
+            <Layout title="Alerts">
+              <Alerts />
+            </Layout>
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/readings"
+        element={
+          <ProtectedRoute>
+            <Layout title="Readings">
+              <Readings />
+            </Layout>
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/users"
+        element={
+          <ProtectedRoute>
+            <Layout title="Users">
+              <Users />
+            </Layout>
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/settings"
+        element={
+          <ProtectedRoute>
+            <Layout title="Settings">
+              <Settings />
+            </Layout>
+          </ProtectedRoute>
+        }
+      />
+
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
+}
+
+export default App;
