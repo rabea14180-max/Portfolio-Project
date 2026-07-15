e170290ee1788bf328081cd78039b58f5381c515
Hamsa1913 <Hamsa12021@gmail.com>
2026-07-16 01:16:12 +0300

    Implement WiFi and HTTP POST for temperature readings
    Added WiFi connection and HTTP POST functionality to send temperature data to the backend server.

--- Stage 4/sensor/esp32_temperature.ino
+++ Stage 4/sensor/esp32_temperature.ino
@@ -1,3 +1,5 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
 #include <DHT.h>
 
 #define DHTPIN 15
@@ -5,10 +7,30 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
+const char* ssid = "Hams";
+const char* password = "12345678";
+
+// Backend URL
+const char* serverUrl = "http://172.20.10.4:5001/api/readings";
+
 void setup() {
   Serial.begin(115200);
   Serial.println("FlexSight Temperature Monitoring Started");
+
   dht.begin();
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi connected!");
+  Serial.print("ESP32 IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
@@ -21,10 +43,29 @@ void loop() {
     Serial.print(temperature);
     Serial.println(" °C");
 
-    if (temperature >= 50) {
-      Serial.println("ALERT: Temperature reached 50°C or higher!");
+    if (WiFi.status() == WL_CONNECTED) {
+      HTTPClient http;
+
+      http.begin(serverUrl);
+      http.addHeader("Content-Type", "application/json");
+
+      
+      String jsonData = "{\"device_id\":4,\"temperature\":";
+      jsonData += String(temperature);
+      jsonData += "}";
+
+      int responseCode = http.POST(jsonData);
+
+      Serial.print("Backend response code: ");
+      Serial.println(responseCode);
+
+      http.end();
+    }
+
+    if (temperature >= 20) {
+      Serial.println("ALERT: Temperature reached 20C or higher!");
     }
   }
 
-  delay(2000);
+  delay(5000);
 }
